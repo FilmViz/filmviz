717193687ba7947287962b4ff60d77604150c62f
Ramón Calderón <contact@rcalderon.es>
2015-07-13 17:20:02 +0200

    Improve color visualizer

--- sample-code/colorVisualizer/colorVisualizer.js
+++ sample-code/colorVisualizer/colorVisualizer.js
@@ -1,153 +1,155 @@
 var colorVisualizer = (function() {
-  return {
-    showPatch: function (colors, targetElt) {
-      var patchSize = 40,
+  var init = function(colors, targetElt) {
+    var patchSize = 40,
         width = patchSize * colors.length,
-        height = patchSize,
-        svg = document.querySelector(targetElt + ' svg');
+        height = patchSize;
 
-      if (!svg) {
-        svg = d3.select(targetElt)
-          .append('svg')
+    var svg = d3.selection(targetElt).select('svg');
+
+    if (svg.empty()) {
+      svg = d3.selection(targetElt)
+        .append('svg')
           .attr('width', '100%')
-          .attr('viewBox','0 0 ' + width + ' ' + height)
           .attr('preserveAspectRatio','xMinYMin');
-      } else {
-        svg = d3.select(targetElt).select('svg')
-          .attr('viewBox','0 0 ' + width + ' ' + height);
-      }
+    }
 
-      var patch = svg.selectAll('rect')
-        .data(colors);
+    svg.attr('viewBox','0 0 ' + width + ' ' + height);
 
-      patch.enter()
-        .append('rect')
-          .style('opacity', 0)
-          .attr('width', patchSize)
-          .attr('height', patchSize)
-          .attr('x', function(d, i) {
-            return patchSize * i;
-          })
-          .transition();
+    var scale = d3.scale.ordinal()
+      .domain(d3.range(0, colors.length))
+      .rangeBands([0, width]);
 
-      patch
-        .transition()
-        .style('fill', function(d) {
-          return d;
-        })
+    return {svg: svg, scale: scale};
+  }
+  
+  return {
+    showPatchRow: function(colors, targetElt) {
+      var param = init(colors, targetElt),
+          svg = param.svg,
+          scale = param.scale,
+          patch = svg.selectAll('rect').data(colors);
+
+      patch.enter().append('rect')
+        .attr('width', scale.rangeBand())
+        .attr('height', scale.rangeBand())
+        .attr('x', function(d, i) { return scale(i); })
+        .style('opacity', 0);
+
+      patch.transition()
+        .style('fill', function(d) { return d; })
         .style('opacity', 1);
 
-      patch.exit()
-        .transition()
+      patch.exit().transition()
         .style('opacity', 0)
         .remove();
     },
 
-    showAccordion: function (colors, targetElt) {
-      var patchSize = 40,
-        width = patchSize * colors.length,
-        height = patchSize
-        padding = 0,
-        svg = document.querySelector(targetElt + ' svg');
+    showAccordionPatchRow: function(colors, targetElt) {
+      var param = init(colors, targetElt),
+          svg = param.svg,
+          scale = param.scale,
+          patch = svg.selectAll('g').data(colors),
+          leftPatches = [],
+          rightPatches = [];
 
-      var leftPatch = [];
-      var rightPatch = [];
+      var patchEnter = patch.enter().append('g')
+        .style('opacity', 0)
+        .attr('transform', function(d, i) {
+          return 'translate(' + scale(i) + ', 0)';
+        });
 
-      var xScale = d3.scale.ordinal()
-                  .domain(d3.range(0, colors.length))
-                  .rangeBands([0, width], padding);
+      patchEnter.append('rect')
+        .attr('width', scale.rangeBand())
+        .attr('height', scale.rangeBand());
 
-      if (!svg) {
-        svg = d3.select(targetElt)
-          .append('svg')
-          .attr('width', '100%')
-          .attr('viewBox','0 0 ' + width + ' ' + height)
-          .attr('preserveAspectRatio','xMinYMin');
-      } else {
-        svg = d3.select(targetElt).select('svg')
-          .attr('viewBox','0 0 ' + width + ' ' + height);
-      }
+      patchEnter.append('text')
+        .attr('dx', 0)
+        .attr('dy', scale.rangeBand() * 3 / 5)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '12')
+        .style('fill', 'white')
+        .style('opacity', 0);
 
-      var patch = svg.selectAll('g')
-        .data(colors);
-
-      var patchEnter = patch
-        .enter()
-        .append('g')
-          .style('opacity', 0)
-          .attr('transform', function (d, i) {
-            return 'translate(' + xScale(i) + ', 0)';
-          });
-
-      patchEnter
-        .append('rect')
-          .attr('width', xScale.rangeBand())
-          .attr('height', patchSize);
-
-      patchEnter
-        .append('text')
-          .attr('dy', patchSize * 3 / 5)
-          .attr('font-family', 'sans-serif')
-          .attr('font-size', '12')
-          .style('fill', 'white')
-          .style('opacity', 0);
-
-      patch
-        .select('rect')
-          .transition()
-          .style('fill', function (d) { return d; });
-
-      patch
-        .select('text')
-          .text(function (d) { return d; });
-
-      patch
+      patch.select('rect')
         .transition()
+        .style('fill', function(d) { return d; });
+
+      patch.select('text')
+        .text(function(d) { return d; });
+
+      patch.transition()
         .style('opacity', 1);
 
-      patch
-        .exit()
+      patch.exit()
           .style('opacity', 0)
           .remove();
 
-      patch.on('mouseover', function (d, i) {
-        leftPatch = patch.select('rect')[0].slice(0, i);
-        rightPatch = patch.select('rect')[0].slice(i + 1);
+      patch.on('mouseover', function(d, i) {
+        leftPatches = patch.select('rect')[0].slice(0, i);
+        rightPatches = patch.select('rect')[0].slice(i + 1);
 
-        d3.selectAll(leftPatch)
-        .transition().ease('cubic-in-out')
-          .attr('transform', 'translate(' + -xScale.rangeBand() + ', 0)');
+        if (leftPatches.length === 0) {
+          d3.selectAll(rightPatches)
+            .transition().ease('cubic-in-out')
+            .attr('transform', 'translate(' + scale.rangeBand() + ', 0)');
 
-        d3.selectAll(rightPatch)
-        .transition().ease('cubic-in-out')
-          .attr('transform', 'translate(' + xScale.rangeBand() + ', 0)');
+          d3.select(this).select('rect')
+            .transition().ease('cubic-in-out')
+            .attr('width', scale.rangeBand() * 4);
 
-        d3.select(this).select('rect')
-          .transition().ease('cubic-in-out')
-          .attr('width', xScale.rangeBand() * 4)
-          .attr('transform', 'translate(' + -xScale.rangeBand() + ', 0)');
+          d3.select(this).select('text')
+            .attr('dx', scale.rangeBand() / 2);
+        } else if (rightPatches.length === 0) {
+          d3.selectAll(leftPatches)
+            .transition().ease('cubic-in-out')
+            .attr('transform', 'translate(' + -scale.rangeBand() + ', 0)');
+
+          d3.select(this).select('rect')
+            .transition().ease('cubic-in-out')
+            .attr('width', scale.rangeBand() * 4)
+            .attr('transform', 'translate(' + -scale.rangeBand() + ', 0)');
+
+          d3.select(this).select('text')
+            .attr('dx', -scale.rangeBand() / 2);
+        } else {
+          d3.selectAll(leftPatches)
+            .transition().ease('cubic-in-out')
+            .attr('transform', 'translate(' + -scale.rangeBand() / 2 + ', 0)');
+
+          d3.selectAll(rightPatches)
+            .transition().ease('cubic-in-out')
+            .attr('transform', 'translate(' + scale.rangeBand() / 2 + ', 0)');
+
+          d3.select(this).select('rect')
+            .transition().ease('cubic-in-out')
+            .attr('width', scale.rangeBand() * 2)
+            .attr('transform', 'translate(' + -scale.rangeBand() / 2 + ', 0)');
+        }
+
+        patch.select('text')
+          .text(function(d) { return d; });
 
         d3.select(this).select('text')
           .transition()
           .style('opacity', 1);
       });
 
-      patch.on('mouseout', function (d, i) {
-        leftPatch = patch.select('rect')[0].slice(0, i);
-        rightPatch = patch.select('rect')[0].slice(i + 1);
+      patch.on('mouseout', function(d, i) {
+        leftPatches = patch.select('rect')[0].slice(0, i);
+        rightPatches = patch.select('rect')[0].slice(i + 1);
 
-        d3.selectAll(leftPatch)
+        d3.selectAll(leftPatches)
         .transition().ease('cubic-in-out')
           .attr('transform', 'translate(0, 0)');
 
-        d3.selectAll(rightPatch)
+        d3.selectAll(rightPatches)
         .transition().ease('cubic-in-out')
           .attr('transform', 'translate(0, 0)');
 
         d3.select(this).select('rect')
         .transition().ease('cubic-in-out')
-          .attr('transform', 'translate(0, 0)')
-          .attr('width', xScale.rangeBand());
+          .attr('width', scale.rangeBand())
+          .attr('transform', 'translate(0, 0)');
 
         d3.select(this).select('text')
           .transition()
