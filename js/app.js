417514d632662db61b4ab1b73e43404bc9363ea2
surreal6 <palidoestudio2@gmail.com>
2015-07-15 12:29:46 +0200

    starting to implement routes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
 (function() {
 	
-	angular.module('FilmVizApp', []);
+	angular.module('FilmVizApp', ['ngRoute']);
+
+	// configure our routes
+  	angular.module('FilmVizApp').config(function($routeProvider) {
+  		$routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/main.html',
+                controller  : 'mainCtrl'
+            })
+
+            // route for the home page
+            .when('/init', {
+                templateUrl : 'pages/init.html',
+                controller  : 'mainCtrl'
+            })
+
+            // route for the about page
+            .when('/editor', {
+                templateUrl : 'pages/editor.html',
+                controller  : 'aboutCtrl'
+            })
+
+            // route for the contact page
+            .when('/preview', {
+                templateUrl : 'pages/preview.html',
+                controller  : 'contactCtrl'
+            });
+    });
+
 
 
 }());
