86e03dae4c5e647b957e6c9914d91a7286b6cfb2
surreal6 <palidoestudio2@gmail.com>
2015-07-22 18:02:17 +0200

    fix #39 separate d3 calls into directives

---
+++ js/directives/frameViz.js
@@ -0,0 +1,18 @@
+(function() {
+	
+	angular.module('filmVizApp')
+		.directive('frameViz', function () {
+			return {
+				restrict: 'E',
+				templateUrl: 'js/directives/templates/frameViz.html',
+				link: function(scope, element, attributes) {
+					
+
+					// something
+				
+		    	}
+
+		   
+			};
+		});
+}());
---
+++ js/directives/timelineViz.js
@@ -0,0 +1,29 @@
+(function() {
+	
+	angular.module('filmVizApp')
+		.directive('timelineViz', function () {
+			return {
+				restrict: 'E',
+				templateUrl: 'js/directives/templates/timelineViz.html',
+				link: function(scope, element, attributes) {
+					
+					var svg = document.querySelector("div.timeline-viz svg");
+
+					switch (project.selectedAnalysis) {
+						case 0:  // Color analysis
+							break;
+						default:
+							console.log("Patata");
+					}
+					scope.pepe = function () {
+						console.log("esta es la ejecucion", svg);
+					};
+
+					scope.pepe();
+					
+		    }
+
+		   
+			};
+		});
+}());
