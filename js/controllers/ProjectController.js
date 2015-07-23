0fd77231f5f6af547cdff0485a14f408f1df0b06
Ramón Calderón <contact@rcalderon.es>
2015-07-23 21:14:38 +0200

    Add first rough version of color timeline visualization

--- js/controllers/ProjectController.js
+++ js/controllers/ProjectController.js
@@ -1,5 +1,4 @@
 (function() {
-
   angular.module('filmVizApp')
     .controller('ProjectController', function() {
       this.project = project;
@@ -161,4 +160,38 @@
   //     ]
   //    };
 
+=======
+    name: "prueba",
+    videosrc: "video.mp4",
+    selectedAnalysis: 0,
+    analysis: [
+      {
+        name: "color",
+        isDone: false,
+        data: []
+      },
+      {
+        name: "audio",
+        isDone: false,
+        data: []
+      },
+      {
+        name: "motion",
+        isDone: false,
+        data: []
+      },
+      {
+        name: "metadata1",
+        isDone: false,
+        tags: ["perro", "casa", "etiqueta"],
+        data: []
+      },
+      {
+        name: "Escalas de Plano",
+        isDone: false,
+        tags: ["Plano Detalle", "Primerisimo Primer Plano", "Primer Plano", "Plano Corto", "Plano Medio", "Plano Americano", "Plano Largo", "Plano General", "Gran Plano General"],
+        data:[]
+      }
+    ]
+  };
 }());
--- js/directives/frameViz.js
+++ js/directives/frameViz.js
@@ -1,13 +1,12 @@
 (function() {
-
-  angular.module('filmVizApp')
+	angular.module('filmVizApp')
 		.directive('frameViz', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'js/directives/templates/frameViz.html',
-    link: function(scope, element, attributes) {
-      // something
-    }
-  };
+			return {
+				restrict: 'E',
+				templateUrl: 'js/directives/templates/frameViz.html',
+				link: function(scope, element, attributes) {
+					// something
+        }
+      };
 		});
 }());
