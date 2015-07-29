b9bf41abdc0ef75aaeb4f9756aee5b3f2c1e659a
Ramón Calderón <contact@rcalderon.es>
2015-07-29 23:21:11 +0200

    Temporal changes in color visualizations so that they work with analyzer button

--- js/directives/frameColorViz.js
+++ js/directives/frameColorViz.js
@@ -5,9 +5,6 @@
         restrict: 'E',
         templateUrl: 'js/directives/templates/frameColorViz.html',
         link: function(scope, element, attributes) {
-        setTimeout(function() {
-          console.log('Attaching!');
-
           // Function adapted from
           // www.runtime-era.com/2011/11/grouping-html-hex-colors-by-hue-in.html
           var sortColors = function(a, b, sortingMode) {
@@ -56,49 +53,57 @@
             }
           };
           
-          var width = d3.select('div.frame-viz').node().offsetWidth;
-          var height = d3.select('div.frame-viz').node().offsetHeight;
-          var svg = d3.select('#frame-color-viz')
-            .attr('preserveAspectRatio', 'none')
-            .attr('viewBox', '0 0 ' + width + ' ' + height);
-
-          var scale = d3.scale.ordinal()
-            .rangeBands([0, width]);
-
-          var video = document.getElementById('video');
-          var track = video.textTracks[0];
-
           scope.updateColorViz = function(sortingMode) {
+            var video = document.getElementById('video');
+            var track = video.textTracks[0];
+            var colors = JSON.parse(track.activeCues[0].text).colors;
+
+            var width = d3.select('div.frame-viz').node().offsetWidth;
+            var svg = d3.select('#frame-color-viz');
+
+            var scale = d3.scale.ordinal()
+              .rangeBands([0, width])
+              .domain(d3.range(0, colors.length));
+
             svg.selectAll('rect')
               .sort(function(a, b) { return sortColors(a, b, sortingMode); })
               .transition().duration(500)
               .attr('x', function(d, i) { return scale(i); });
           };
-
-          track.addEventListener('cuechange', function() {
-            var colors = JSON.parse(track.activeCues[0].text).colors;
-
-            scale.domain(d3.range(0, colors.length));
-
-            var patch = svg.selectAll('rect')
-              .data(colors);
-
-            patch.enter().append('rect')
-              .attr('width', scale.rangeBand())
-              .attr('height', scale.rangeBand())
-              .attr('x', function(d, i) { return scale(i); })
-              .style('opacity', 0);
-
-            patch.transition()
-              .style('fill', function(d) { return d; })
-              .style('opacity', 1);
-
-            patch.exit().transition()
-              .style('opacity', 0)
-              .remove();
-          });
-          }, 10000);
         }
       };
     });
 }());
+
+var showFrameColorViz = function() {
+  var video = document.getElementById('video');
+  var track = video.textTracks[0];
+  var colors = JSON.parse(track.activeCues[0].text).colors;
+
+  var width = d3.select('div.frame-viz').node().offsetWidth;
+  var height = d3.select('div.frame-viz').node().offsetHeight;
+  var svg = d3.select('#frame-color-viz')
+    .attr('preserveAspectRatio', 'none')
+    .attr('viewBox', '0 0 ' + width + ' ' + height);
+
+  var scale = d3.scale.ordinal()
+    .rangeBands([0, width])
+    .domain(d3.range(0, colors.length));
+
+  var patch = svg.selectAll('rect')
+    .data(colors);
+
+  patch.enter().append('rect')
+    .attr('width', scale.rangeBand())
+    .attr('height', scale.rangeBand())
+    .attr('x', function(d, i) { return scale(i); })
+    .style('opacity', 0);
+
+  patch.transition()
+    .style('fill', function(d) { return d; })
+    .style('opacity', 1);
+
+  patch.exit().transition()
+    .style('opacity', 0)
+    .remove();
+};
--- js/directives/timelineColorViz.js
+++ js/directives/timelineColorViz.js
@@ -54,50 +54,63 @@
           };
 
           scope.updateColorViz = function(sortingMode) {
+            var colors = project.analysis[0].data;
+            var patch = d3.select('svg#timeline-color-viz')
+              .selectAll('g').selectAll('rect');
+            var height = d3.select('div.timeline-viz').node().offsetHeight;
+            var patchesPerCol = d3.max(colors, function(d) {
+              return d.content.colors.length;
+            });
+
+            var yScale = d3.scale.ordinal()
+              .domain(d3.range(0, patchesPerCol))
+              .rangeBands([0, height]);
+
             patch.sort(function(a, b) { return sortColors(a, b, sortingMode); })
               .transition().duration(500)
               .attr('y', function(d, i) { return yScale(i); });
           };
-
-          var colors = project.analysis[0].data;
-
-          var width = d3.select('div.timeline-viz').node().offsetWidth;
-          var height = d3.select('div.timeline-viz').node().offsetHeight;
-
-          var svg = d3.select('svg#timeline-color-viz')
-            .attr('preserveAspectRatio','none')
-            .attr('viewBox', '0 0 ' + width + ' ' + height);
-          
-          var patchesPerCol = d3.max(colors, function(d) {
-            return d.content.colors.length;
-          });
-
-          var xScale = d3.scale.ordinal()
-            .domain(d3.range(0, colors.length))
-            .rangeBands([0, width]);
-          
-          var yScale = d3.scale.ordinal()
-            .domain(d3.range(0, patchesPerCol))
-            .rangeBands([0, height]);
-
-          var patchCol = svg.selectAll('g')
-            .data(colors);
-
-          patchCol.enter().append('g')
-            .attr('transform', function(d, i) {
-              return 'translate(' + i * xScale.rangeBand() + ', 0)';
-            });
-
-          var patch = patchCol.selectAll('rect')
-            .data(function(d) { return d.content.colors; });
-
-          patch.enter().append('rect')
-            .attr('width', xScale.rangeBand())
-            .attr('height', yScale.rangeBand())
-            .style('fill', function(d) { return d; });
-
-          scope.updateColorViz('val');
         }
       };
     });
 }());
+
+var showTimelineColorViz = function() {
+  var colors = project.analysis[0].data;
+
+  var width = d3.select('div.timeline-viz').node().offsetWidth;
+  var height = d3.select('div.timeline-viz').node().offsetHeight;
+
+  var svg = d3.select('svg#timeline-color-viz')
+    .attr('preserveAspectRatio','none')
+    .attr('viewBox', '0 0 ' + width + ' ' + height);
+  
+  var patchesPerCol = d3.max(colors, function(d) {
+    return d.content.colors.length;
+  });
+
+  var xScale = d3.scale.ordinal()
+    .domain(d3.range(0, colors.length))
+    .rangeBands([0, width]);
+  
+  var yScale = d3.scale.ordinal()
+    .domain(d3.range(0, patchesPerCol))
+    .rangeBands([0, height]);
+
+  var patchCol = svg.selectAll('g')
+    .data(colors);
+
+  patchCol.enter().append('g')
+    .attr('transform', function(d, i) {
+      return 'translate(' + i * xScale.rangeBand() + ', 0)';
+    });
+
+  var patch = patchCol.selectAll('rect')
+    .data(function(d) { return d.content.colors; });
+
+  patch.enter().append('rect')
+    .attr('width', xScale.rangeBand())
+    .attr('height', yScale.rangeBand())
+    .attr('y', function(d, i) { return yScale(i); })
+    .style('fill', function(d) { return d; });
+};
