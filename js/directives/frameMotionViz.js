e63523970ccece357136fadbd44f623b56af5853
Ramón Calderón <contact@rcalderon.es>
2015-07-30 16:11:54 +0200

    Add frame motion visualization

--- js/directives/frameMotionViz.js
+++ js/directives/frameMotionViz.js
@@ -3,7 +3,28 @@
     .directive('frameMotionViz', function() {
       return {
         restrict: 'E',
-        templateUrl: 'js/directives/templates/frameMotionViz.html'
+        templateUrl: 'js/directives/templates/frameMotionViz.html',
+        link: function(scope, element, attributes) {
+          var width = d3.select('div.frame-viz').node().offsetWidth;
+          var height = d3.select('div.frame-viz').node().offsetHeight;
+          // d3.select('#frame-motion-viz')
+          //   .attr('preserveAspectRatio', 'none')
+          //   .attr('viewBox', '0 0 ' + width + ' ' + height);
+        }
       };
     });
 }());
+
+var showFrameMotionViz = function() { 
+  var video = document.getElementById('video');
+  var track = video.textTracks[2];
+  var motion = JSON.parse(track.activeCues[0].text).motion;
+
+  var div = d3.select('#frame-motion-viz');
+  if (motion > 0.9) {
+    div.style('color', 'red');
+  } else {
+    div.style('color', '#337ab7');
+  }
+  div.text(motion.toFixed(3));
+}
