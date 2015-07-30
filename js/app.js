38ac9c681b80ef2ae0ac4ba81312a02c288bd9a2
surreal6 <carlos@kinoraw.net>
2015-07-30 16:26:30 +0200

    remove routes and modifiy gitignore

--- js/app.js
+++ js/app.js
@@ -7,24 +7,26 @@
     $routeProvider
       .when('/', {
         // route for the home page
-        templateUrl: 'pages/main.html',
-        controller: 'ProjectController'
-      })
-      .when('/init', {
-        // route for the project initialization page
-        templateUrl: 'pages/init.html',
-        controller: 'ProjectController'
-      })
-      .when('/editor', {
-        // route for the project editor page
+        //templateUrl: 'pages/main.html',
         templateUrl: 'pages/editor.html',
         controller: 'ProjectController'
       })
-      .when('/preview', {
-        // route for the full project preview page
-        templateUrl: 'pages/preview.html',
-        controller: 'ProjectController'
-      });
+      
+      // .when('/init', {
+      //   // route for the project initialization page
+      //   templateUrl: 'pages/init.html',
+      //   controller: 'ProjectController'
+      // })
+      // .when('/editor', {
+      //   // route for the project editor page
+      //   templateUrl: 'pages/editor.html',
+      //   controller: 'ProjectController'
+      // })
+      // .when('/preview', {
+      //   // route for the full project preview page
+      //   templateUrl: 'pages/preview.html',
+      //   controller: 'ProjectController'
+      // });
   });
 
   /*
