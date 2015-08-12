46949480c27e921b0d3bdfce238525edb27390c2
Ramón Calderón <contact@rcalderon.es>
2015-08-12 13:06:13 +0200

    Finish implementation of promises for analyzer

--- scripts/services/analyzer.js
+++ scripts/services/analyzer.js
@@ -13,7 +13,7 @@ angular.module('filmViz')
       canvas.width = video.videoWidth / 4;
 
       var context = canvas.getContext('2d');
-      var interval = 30;
+      var interval = 1;
 
       var colorAnalysis = new ProjectData.Analysis('color');
       var audioAnalysis = new ProjectData.Analysis('audio');
@@ -27,21 +27,26 @@ angular.module('filmViz')
 
       var _this = this;
 
+      var colorTrackPromises = [];
+      var audioTrackPromises = [];
+      var motionTrackPromises = [];
+
       function seekedListener(event) {
+        var cueStartTime = video.currentTime;
 
         // function loopInAnalysis
         console.log('seeked: analyzing frame');
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
         currentImg.src = canvas.toDataURL('image/jpg');
 
-        // Generate color analysis8
-        var colorPromise = Promise.resolve(Color.capturePalette(currentImg, 16));
+        // Generate color analysis
+        var colorFramePromise = Promise.resolve(Color.capturePalette(currentImg, 16));
 
         // Generate audio analysis
-        var audioPromise = Promise.resolve(1);
+        var audioFramePromise = Promise.resolve(1);
 
         // Generate motion analysis
-        var motionPromise = new Promise(function(resolve, reject) {
+        var motionFramePromise = new Promise(function(resolve, reject) {
           if (!lastSrcImg) {
             resolve(0);
           } else {
@@ -51,65 +56,76 @@ angular.module('filmViz')
           }
         });
 
+        colorFramePromise = colorFramePromise.then(function(value) {
+          return new ProjectData.Cue(value, cueStartTime);
+        });
+
+        audioFramePromise = audioFramePromise.then(function(value) {
+          return new ProjectData.Cue(value, cueStartTime);
+        });
+
+        motionFramePromise = motionFramePromise.then(function(value) {
+          return new ProjectData.Cue(value, cueStartTime);
+        });
+
         // create cueObjects and store in each analysis
-        colorPromise.then(function(colorPalette) {
-          var colorCue = new ProjectData.Cue(colorPalette, video.currentTime);
-          colorAnalysis.data.push(colorCue);
-        });
+        colorTrackPromises.push(colorFramePromise);
+        audioTrackPromises.push(audioFramePromise);
+        motionTrackPromises.push(motionFramePromise);
 
-        audioPromise.then(function(audio) {
-          var audioCue = new ProjectData.Cue(audio, video.currentTime);
-          audioAnalysis.data.push(audioCue);
-        });
-
-        motionPromise.then(function(motionPercentage) {
-          var motionCue = new ProjectData.Cue(motionPercentage, video.currentTime);
-          motionAnalysis.data.push(motionCue);
-        });
-
-        Promise.all([colorPromise, audioPromise, motionPromise]).then(function(values) {
-          console.log(values);
-        });
-
-        if (video.currentTime < video.duration) {
+        if (video.currentTime < video.duration - interval) {
           video.currentTime += interval;
           lastSrcImg = currentImg.src;
         } else {
-          // function storeAnalysis
           console.log('creating tracks and cues');
-
-          // stop video
           video.pause();
 
-          colorAnalysis.isDone = true;
-          audioAnalysis.isDone = true;
-          motionAnalysis.isDone = true;
+          Promise.all(colorTrackPromises).then(function(cues) {
+            cues.forEach(function(cue) {
+              colorAnalysis.data.push(cue);
+            });
 
-          // analysis is finished, lets save data
-          ProjectData.analysisCollection.push(colorAnalysis);
-          ProjectData.analysisCollection.push(audioAnalysis);
-          ProjectData.analysisCollection.push(motionAnalysis);
+            colorAnalysis.isDone = true;
+            ProjectData.analysisCollection.push(colorAnalysis);
+            var colorTrack = video.addTextTrack('metadata', 'color');
+            _this.addCuesToVideoTrack(colorTrack, colorAnalysis.data, video);
 
-          // create tracks
-          var colorTrack = video.addTextTrack('metadata', 'color');
-          var audioTrack = video.addTextTrack('metadata', 'audio');
-          var motionTrack = video.addTextTrack('metadata', 'motion');
+            colorTrack.addEventListener('cuechange', function() {
+              showFrameColorViz();
+            });
 
-          _this.generateCues(colorTrack, colorAnalysis.data, video);
-          _this.generateCues(audioTrack, audioAnalysis.data, video);
-          _this.generateCues(motionTrack, motionAnalysis.data, video);
-
-          colorTrack.addEventListener('cuechange', function() {
-            showFrameColorViz();
+            showTimelineColorViz(colorAnalysis.data);
           });
 
-          motionTrack.addEventListener('cuechange', function() {
-            showFrameMotionViz();
+          Promise.all(audioTrackPromises).then(function(cues) {
+            cues.forEach(function(cue) {
+              audioAnalysis.data.push(cue);
+            });
+
+            audioAnalysis.isDone = true;
+            ProjectData.analysisCollection.push(audioAnalysis);
+            var audioTrack = video.addTextTrack('metadata', 'audio');
+            _this.addCuesToVideoTrack(audioTrack, audioAnalysis.data, video);
+          });
+
+          Promise.all(motionTrackPromises).then(function(cues) {
+            cues.forEach(function(cue) {
+              motionAnalysis.data.push(cue);
+            });
+
+            motionAnalysis.isDone = true;
+            ProjectData.analysisCollection.push(motionAnalysis);
+            var motionTrack = video.addTextTrack('metadata', 'motion');
+            _this.addCuesToVideoTrack(motionTrack, motionAnalysis.data, video);
+
+            motionTrack.addEventListener('cuechange', function() {
+              showFrameMotionViz();
+            });
+
+            showTimelineMotionViz(motionAnalysis.data);
           });
 
           video.removeEventListener('seeked', seekedListener, false);
-          showTimelineMotionViz(motionAnalysis.data);
-          showTimelineColorViz(colorAnalysis.data);
 
           // analysis finished
           console.log('analysis finished');
@@ -121,13 +137,12 @@ angular.module('filmViz')
 
     // TODO: move to ProjectData.Analysis class
     // TODO: data could be Analysis and call Analysis.data
-    this.generateCues = function(track, data, video) {
+    this.addCuesToVideoTrack = function(track, cueObjs, video) {
       // generate color cues
-      data.forEach(function(cueObj, index, arr) {
+      cueObjs.forEach(function(cueObj, index, arr) {
         var startTime = cueObj.startTime;
         var endTime = (index === arr.length - 1) ? video.duration : arr[index + 1].startTime;
         track.addCue(new VTTCue(startTime, endTime, angular.toJson(cueObj.content)));
       });
     };
-
   },]);
